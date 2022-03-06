ccbe9e7ffb7bc7f44e31d190d106d6bb41345e03
misba0770 <80534061+misba0770@users.noreply.github.com>
2022-03-06 22:39:21 +0530

    Left view of Binary Tree

---
+++ leftViewOfTree.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author misba zaidi
+ */
+public class leftViewOfTree {
+static int max=0;
+    private static void leftView(Node root,int level) {
+        if(root==null)return;
+        if(level>=max){
+            System.out.print(root.data+" ");max++;
+        }
+        leftView(root.left,level+1);
+        leftView(root.right,level+1);
+    }
+
+   static class Node{
+       Node left;
+       Node right;
+       int data;
+       Node(int x){
+           data=x;
+           left=right=null;
+       }
+           
+   }
+    public static void main(String[] args) {
+       Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.right.left = new Node(5);
+        root.right.right = new Node(6);
+        root.right.left.left = new Node(7);
+        root.right.left.right = new Node(8);
+        leftView(root,0);
+    }
+    
+}
